2e7e0953aac78ec82adcac08845bea76690e6059
Florian <floriancunnac@gmail.com>
2023-12-05 10:53:56 +0100

    Ajout des classes Porte et Trappe

--- project_name/conf_flo.h
+++ project_name/conf_flo.h
@@ -5,6 +5,14 @@
 #define Pin_Capteur_Ultrason 13
 #define Dist_Max 150
 
+//Trappe à croquettes
+#define Pin_Servo_Trappe 14
+#define Servo_Angle_O 554
+#define Servo_Angle_180 2200
+
+#define Valeur_Ouverture_Porte 90
+#define Valeur_Fermeture_Porte 0
+
 //#define 
 
 #endif
